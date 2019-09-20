edb0c7df405b706aa28d5ea6c4e76c59cb2cb31c
Bernish <hanychak@gmail.com>
2019-09-20 12:34:57 +0300

    second

--- src/main/java/com/epam/student/bernish/Application.java
+++ src/main/java/com/epam/student/bernish/Application.java
@@ -1,11 +1,14 @@
 package com.epam.student.bernish;
 
         import java.util.ArrayList;
+        import java.util.Arrays;
         import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        // epam student
-        System.out.println("Hello123");
+        public static void main(String[] args) {
+            // epam student
+            for (int i = 0; i <= 10; i++)
+                System.out.println("3 * " + i + " = " + 3 * i);
+
     }
 }
