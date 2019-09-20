31b7b8c68dc238e1e0ea3e703918981b22ac2121
Bernish <hanychak@gmail.com>
2019-09-20 11:36:52 +0300

    first

---
+++ src/main/java/com/epam/student/bernish/Application.java
@@ -0,0 +1,11 @@
+package com.epam.student.bernish;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        // epam student
+        System.out.println("Hello123");
+    }
+}
